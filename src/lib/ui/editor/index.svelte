d641308f72f511ae69c6eb67567a0e7024cc4153
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2024-05-26 14:48:56 -0700

    Remove unecessary comment

--- src/lib/ui/editor/index.svelte
+++ src/lib/ui/editor/index.svelte
@@ -18,11 +18,6 @@
 	import Toasts, { addToast } from '../toasts.svelte';
 
 	import EditorBubbleMenu from './bubble-menu/index.svelte';
-
-	/**
-	 * The API route to use for the OpenAI completion API.
-	 * Defaults to "/api/generate".
-	 */
 	/**
 	 * Additional classes to add to the editor container.
 	 * Defaults to "relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white p-12 px-8 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg".
